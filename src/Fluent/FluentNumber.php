f2c8fe558d28c807bf42495ab1bb8ed70b0c5524
Omer Šabić <omer@datalinx.si>
2023-09-23 16:14:36 +0200

    feat(number): implement number, percent and money formatting

--- src/Fluent/FluentNumber.php
+++ src/Fluent/FluentNumber.php
@@ -2,21 +2,41 @@
 
 namespace DataLinx\PhpUtils\Fluent;
 
+use InvalidArgumentException;
+use NumberFormatter;
+use RuntimeException;
+
 class FluentNumber
 {
+    const TYPE_DECIMAL = 'd';
+    const TYPE_INT = 'i';
+
     /**
-     * @var float Number value
+     * @var float|int|string Number value
      */
-    protected float $value;
+    protected $value;
+
+    /**
+     * @var string Number type (see class constants)
+     */
+    protected string $type;
 
     /**
      * Create a FluentNumber object
      *
-     * @param float $value
+     * @param float|int|string $value
      */
-    public function __construct(float $value)
+    public function __construct($value)
     {
         $this->value = $value;
+
+        if (is_int($value)) {
+            $this->type = self::TYPE_INT;
+        } elseif (is_numeric($value)) {
+            $this->type = self::TYPE_DECIMAL;
+        } else {
+            throw new InvalidArgumentException('Value is not numeric');
+        }
     }
 
     /**
@@ -24,7 +44,7 @@ class FluentNumber
      */
     public function __toString(): string
     {
-        return (string)$this->value;
+        return $this->format();
     }
 
     /**
@@ -88,5 +108,124 @@ class FluentNumber
 
         // The Roman numeral should be built, return it
         return new FluentString($result);
+    }
+
+    /**
+     * Was the object instance created with an integer value?
+     *
+     * @return bool
+     */
+    public function isInteger(): bool
+    {
+        return $this->type === self::TYPE_INT;
+    }
+
+    /**
+     * Was the object instance created with a decimal value?
+     *
+     * @return bool
+     */
+    public function isDecimal(): bool
+    {
+        return $this->type === self::TYPE_DECIMAL;
+    }
+
+    /**
+     * Get NumberFormatter instance
+     *
+     * @param string|null $locale Specific locale or null to use the current locale
+     * @param int|null $style NumberFormatter style parameter or null to use the default style
+     * @param int|null $decimals Number of decimals to show
+     * @param int|null $trim_to_decimals Trim trailing decimal zeros but always keep the specified number of decimals
+     * @return NumberFormatter
+     */
+    protected function getFormatter(?string $locale, ?int $style, ?int $decimals, ?int $trim_to_decimals): NumberFormatter
+    {
+        $formatter = new NumberFormatter($locale ?: setlocale(LC_MESSAGES, '0'), $style ?? NumberFormatter::DEFAULT_STYLE);
+
+        if ($decimals === null) {
+            // Use default number of decimal digits
+            $formatter->setAttribute(NumberFormatter::MIN_FRACTION_DIGITS, $trim_to_decimals ?? ($this->isInteger() ? 0 : 2));
+            $formatter->setAttribute(NumberFormatter::MAX_FRACTION_DIGITS, 10);
+        } else {
+            $formatter->setAttribute(NumberFormatter::MIN_FRACTION_DIGITS, $trim_to_decimals ?? $decimals);
+            $formatter->setAttribute(NumberFormatter::MAX_FRACTION_DIGITS, $decimals);
+        }
+
+        return $formatter;
+    }
+
+    /**
+     * Format the number as a string.
+     *
+     * By default, formatting depends on the type of value that you created the FluentNumber object with (integer or float).
+     *
+     * **Please note!** By default for decimals numbers, up to 10 decimal digits are shown, unless you provide a value for the `decimals` parameter.
+     *
+     * @param int|null $decimals (Optional) Number of decimal digits to show. Defaults to 2 for decimals numbers.
+     * @param int|null $trim_to_decimals (Optional) Trim trailing decimal zeros but always keep the specified number of decimals
+     * @param string|null $locale (Optional) Override the locale
+     * @return string
+     */
+    public function format(?int $decimals = NULL, ?int $trim_to_decimals = NULL, ?string $locale = NULL): string
+    {
+        $formatter = $this->getFormatter($locale, null, $decimals, $trim_to_decimals);
+
+        $str = $formatter->format($this->value);
+
+        if ($str === false) {
+            throw new RuntimeException('NumberFormatter::format() error: '. $formatter->getErrorMessage() ." ({$formatter->getErrorCode()})");
+        }
+
+        return $str;
+    }
+
+    /**
+     * Format the number as a percentage.
+     *
+     * By default, formatting depends on the type of value that you created the FluentNumber object with (integer or float).
+     *
+     * **Please note!** By default for decimals numbers, up to 10 decimal digits are shown, unless you provide a value for the `decimals` parameter.
+     *
+     * @param int|null $decimals (Optional) Number of decimal digits to show. Defaults to 2 for decimals numbers.
+     * @param int|null $trim_to_decimals (Optional) Trim trailing decimal zeros but always keep the specified number of decimals
+     * @param string|null $locale (Optional) Override the locale
+     * @return string
+     */
+    public function asPercent(?int $decimals = NULL, ?int $trim_to_decimals = NULL, ?string $locale = NULL): string
+    {
+        $formatter = $this->getFormatter($locale, NumberFormatter::PERCENT, $decimals, $trim_to_decimals);
+
+        $str = $formatter->format($this->value / 100);
+
+        if ($str === false) {
+            throw new RuntimeException('NumberFormatter::format() error: '. $formatter->getErrorMessage() ." ({$formatter->getErrorCode()})");
+        }
+
+        return $str;
+    }
+
+    /**
+     * Format the number as a money amount.
+     *
+     * **Please note!** By default for decimals numbers, up to 10 decimal digits are shown, unless you provide a value for the `decimals` parameter.
+     *
+     * @param string $currency Currency ISO code (EUR, USD, CAD...)
+     * @param int|null $decimals (Optional) Number of decimal digits to show. Defaults to 2 for decimals numbers.
+     * @param int|null $trim_to_decimals (Optional) Trim trailing decimal zeros but always keep the specified number of decimals
+     * @param string|null $locale (Optional) Override the locale
+     * @return string
+     */
+    public function asMoney(string $currency, ?int $decimals = NULL, ?int $trim_to_decimals = NULL, ?string $locale = NULL): string
+    {
+        $formatter = $this->getFormatter($locale, NumberFormatter::CURRENCY, $decimals, $trim_to_decimals);
+
+        $str = $formatter->formatCurrency($this->value, $currency);
+
+        if ($str === false) {
+            throw new RuntimeException('NumberFormatter::formatCurrency() error: '. $formatter->getErrorMessage() ." ({$formatter->getErrorCode()})");
+        }
+
+        return $str;
     }
 }
--- tests/Unit/Fluent/FluentNumberTest.php
+++ tests/Unit/Fluent/FluentNumberTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace DataLinx\PhpUtils\Tests\Unit\Fluent;
 
+use InvalidArgumentException;
 use PHPUnit\Framework\TestCase;
 
 class FluentNumberTest extends TestCase
@@ -29,7 +30,14 @@ class FluentNumberTest extends TestCase
     {
         $amount = num(55);
 
-        $this->assertEquals("5545", $amount . 45);
+        $this->assertEquals('5545', $amount . 45);
+
+        // Change locale
+        setlocale(LC_MESSAGES, 'sl_SI');
+
+        $decimal = num(123.456);
+
+        $this->assertEquals('123,456', (string)$decimal);
     }
 
     /**
@@ -61,5 +69,188 @@ class FluentNumberTest extends TestCase
         foreach ($cases as $case => $expected) {
             $this->assertEquals($expected, num($case)->toRoman());
         }
+    }
+
+    public function testTypes(): void
+    {
+        // Valid integers
+        $this->assertTrue(num(123)->isInteger());
+        $this->assertTrue(num(-123)->isInteger());
+        $this->assertTrue(num((int)'123')->isInteger());
+        $this->assertTrue(num((int)'-123')->isInteger());
+
+        // Invalid integers
+        $this->assertFalse(num(123.45)->isInteger());
+        $this->assertFalse(num('123.45')->isInteger());
+
+        // Valid decimals
+        $this->assertTrue(num(123.45)->isDecimal());
+        $this->assertTrue(num('123.45')->isDecimal());
+        $this->assertTrue(num('123')->isDecimal());
+
+        // Invalid decimals
+        $this->assertFalse(num(123)->isDecimal());
+    }
+
+    public function testNonNumericType(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+
+        num('abc');
+    }
+
+    public function testStringWithNumberType(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+
+        num('abc123');
+    }
+
+    public function testNumberWithStringType(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+
+        num('123abc');
+    }
+
+    public function testFormat(): void
+    {
+        // American style
+        // ------------------------------
+        setlocale(LC_MESSAGES, 'en_US');
+
+        // Integers
+        $integer = num(1234567);
+        $this->assertEquals('1,234,567', (string)$integer);
+        $this->assertEquals('1,234,567.00', $integer->format(2));
+
+        // Decimals
+        $decimal = num(1234.56789);
+        $this->assertEquals('1,234.56789', (string)$decimal);
+        $this->assertEquals('1,234.56789', $decimal->format(5));
+        $this->assertEquals('1,234.567890', $decimal->format(6));
+        $this->assertEquals('1,234.57', $decimal->format(2));
+        $this->assertEquals('1,235', $decimal->format(0));
+
+        $this->assertEquals('1.23456789', (string)num(1.23456789));
+        $this->assertEquals('1.2345678987', (string)num(1.2345678987));
+        $this->assertEquals('1.2345678988', (string)num(1.23456789876));
+        $this->assertEquals('1.23456789876', num(1.23456789876)->format(11));
+
+        // Decimals with trimming
+        $trimmed = num(123.45);
+        $this->assertEquals('123.45000', $trimmed->format(5));
+        $this->assertEquals('123.45', $trimmed->format(5, 2));
+        $this->assertEquals('123.45', $trimmed->format(null, 2));
+
+        $trimmed_2 = num(1234.000);
+        $this->assertEquals('1,234.00', (string)$trimmed_2);
+        $this->assertEquals('1,234.000', $trimmed_2->format(3));
+        $this->assertEquals('1,234', $trimmed_2->format(3, 0));
+        $this->assertEquals('1,234.00', $trimmed_2->format(3, 2));
+
+        // Other styles
+        // ------------------------------
+        // Common EU style
+        setlocale(LC_MESSAGES, 'de_DE');
+
+        $this->assertEquals('1.234.567', (string)$integer);
+        $this->assertEquals('1.234,56789', (string)$decimal);
+
+        // Frenchmen use Narrow non-breaking space for thousands separator
+        setlocale(LC_MESSAGES, 'fr_FR');
+
+        $this->assertEquals("1\u{202F}234\u{202F}567", (string)$integer);
+        $this->assertEquals("1\u{202F}234,56789", (string)$decimal);
+
+        // Override locale on-the-fly
+        // ------------------------------
+        setlocale(LC_MESSAGES, 'en_US');
+
+        // Short locale notation
+        $this->assertEquals('1.234.567', $integer->format(null, null, 'sl'));
+        $this->assertEquals('1.234,56789', $decimal->format(null, null, 'sl'));
+
+        // Long locale notation
+        $this->assertEquals('1.234.567', $integer->format(null, null, 'sl_SI'));
+        $this->assertEquals('1.234,56789', $decimal->format(null, null, 'sl_SI'));
+    }
+
+    public function testAsPercent(): void
+    {
+        setlocale(LC_MESSAGES, 'en_US');
+
+        $value = num(75);
+
+        $this->assertEquals('75%', $value->asPercent());
+        $this->assertEquals('75.00%', $value->asPercent(2));
+        $this->assertEquals('75.000%', $value->asPercent(3));
+
+        $value = num(75.00);
+
+        $this->assertEquals('75.00%', $value->asPercent());
+        $this->assertEquals('75.00%', $value->asPercent(2));
+        $this->assertEquals('75.000%', $value->asPercent(3));
+
+        $value = num(123.456);
+
+        $this->assertEquals('123.456%', $value->asPercent());
+        $this->assertEquals('123.46%', $value->asPercent(2));
+        $this->assertEquals('123.456%', $value->asPercent(3));
+
+        $value = num(123.45678987655);
+
+        $this->assertEquals('123.4567898766%', $value->asPercent());
+        $this->assertEquals('123.46%', $value->asPercent(2));
+        $this->assertEquals('123.456789876550%', $value->asPercent(12));
+
+        $value = num(-123.456);
+
+        $this->assertEquals('-123.456%', $value->asPercent());
+        $this->assertEquals('-123.46%', $value->asPercent(2));
+        $this->assertEquals('-123.456%', $value->asPercent(3));
+    }
+
+    public function testAsMoney(): void
+    {
+        setlocale(LC_MESSAGES, 'en_US');
+
+        $amount = num(1234567.89);
+
+        // Test formatting
+        $this->assertEquals("€1,234,567.89", $amount->asMoney('EUR'));
+        $this->assertEquals("$1,234,567.89", $amount->asMoney('USD'));
+        $this->assertEquals("₹1,234,567.89", $amount->asMoney('INR'));
+        $this->assertEquals("CN¥1,234,567.89", $amount->asMoney('CNY'));
+        $this->assertEquals("¥1,234,567.89", $amount->asMoney('JPY'));
+        $this->assertEquals("RUR\u{00A0}1,234,567.89", $amount->asMoney('RUR'));
+
+        // Test decimals
+        $this->assertEquals("€1,234.56789", num(1234.56789)->asMoney('EUR'));
+        $this->assertEquals("€1,234.57", num(1234.56789)->asMoney('EUR', 2));
+        $this->assertEquals("€1,234.56789", num(1234.56789)->asMoney('EUR', 5));
+        $this->assertEquals("€1,234.56000", num(1234.56)->asMoney('EUR', 5));
+        $this->assertEquals("€1,234.56", num(1234.56)->asMoney('EUR', 5, 2));
+        $this->assertEquals("€1,234.567", num(1234.567)->asMoney('EUR', 5, 2));
+
+        // Test more locale variants
+        // ----------------------------
+        setlocale(LC_MESSAGES, 'sl_SI');
+
+        $this->assertEquals("1.234.567,89\u{00A0}€", $amount->asMoney('EUR'));
+        $this->assertEquals("1.234.567,89\u{00A0}$", $amount->asMoney('USD'));
+        $this->assertEquals("1.234.567,89\u{00A0}₹", $amount->asMoney('INR'));
+        $this->assertEquals("1.234.567,89\u{00A0}CN¥", $amount->asMoney('CNY'));
+        $this->assertEquals("1.234.567,89\u{00A0}¥", $amount->asMoney('JPY'));
+        $this->assertEquals("1.234.567,89\u{00A0}RUR", $amount->asMoney('RUR'));
+
+        setlocale(LC_MESSAGES, 'ru_RU');
+
+        $this->assertEquals("1\u{00A0}234\u{00A0}567,89\u{00A0}€", $amount->asMoney('EUR'));
+        $this->assertEquals("1\u{00A0}234\u{00A0}567,89\u{00A0}$", $amount->asMoney('USD'));
+        $this->assertEquals("1\u{00A0}234\u{00A0}567,89\u{00A0}₹", $amount->asMoney('INR'));
+        $this->assertEquals("1\u{00A0}234\u{00A0}567,89\u{00A0}CN¥", $amount->asMoney('CNY'));
+        $this->assertEquals("1\u{00A0}234\u{00A0}567,89\u{00A0}¥", $amount->asMoney('JPY'));
+        $this->assertEquals("1\u{00A0}234\u{00A0}567,89\u{00A0}р.", $amount->asMoney('RUR'));
     }
 }
